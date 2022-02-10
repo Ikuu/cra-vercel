c5dd1ea0818605ce9b18bc4a7ac8f95c8a1fd496
Alexander Anderson <alexanderson@starstock.com>
2022-02-10 13:59:33 +0000

    Headline changed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Testing Vercel stuff - GitHub Actions</p>
+        <p>Testing Vercel stuff - GitHub Actions - PR</p>
         <a
           className="App-link"
           href="https://reactjs.org"
